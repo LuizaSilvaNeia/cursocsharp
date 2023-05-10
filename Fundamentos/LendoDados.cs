f3873a07f5f46a83ac2cd2e729f20fe6f96fd546
Luiza Silva Neia <luizaneia05@gmail.com>
2023-05-10 16:51:12 -0300

    operadores aritimeticos

---
+++ Fundamentos/LendoDados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class LendoDados {
+        public static void Executar() {
+            Console.WriteLine("Qual seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Qual sua idade?");
+            // fazendo a conversão de um inteiro
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual sua altura?");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Olá { nome} você tem {idade} anos e tem {altura} de altura" );
+
+            // Console.WriteLine("Olá "+nome+" você tem "+idade+" e "+altura+" de altura.");
+        }
+    }
+        
+}
---
+++ Fundamentos/OperadoresAritmeticos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class OperadoresAritmeticos {
+            /*/public static void Executar() {
+            Console.WriteLine("informe um número: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("informe outro número: ");
+            int n2 = int.Parse(Console.ReadLine());
+            int total = n1 + n2;
+
+            Console.WriteLine($"A some de {n1} + {n2} = {total}");
+            }*/
+
+            public static void Executar() {
+
+            Console.WriteLine("Informe a medida da base: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a altura: ");
+            double a = double.Parse(Console.ReadLine());
+
+            double total = (b * a) / 2;
+            Console.WriteLine($" as dimensões do triângolo são {b} * {a} / 2 = {total}");
+
+        }
+
+        
+    }
+}
